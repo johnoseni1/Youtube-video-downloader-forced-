2d0b893dae8c696f97e2ba56c3b92839277a322a
John Oseni <johnoseni26@gmail.com>
2022-01-14 10:18:14 +0100

    pushed the stuffs that are to be oushed today

---
+++ README.md
@@ -0,0 +1,70 @@
+# 
+A feed video downloader for Youtube
+
+Feedtube is for you if:
+
+- You are a fan of RSS
+- You don't like using the Youtube website for some reason (broken js, privacy, or other) but want to keep a list of channel subscriptions
+- You need to download videos and watch them offline
+- You want to watch videos without ads. Note that, as such, video creators and Youtube don't get paid
+
+
+## Dependencies
+
+[xmlstarlet](http://xmlstar.sourceforge.net) and [youtube-dl](https://github.com/rg3/youtube-dl/) (plus [ffmpeg](http://ffmpeg.org))
+
+
+## Setup
+
+1. Just download `downloadtube.sh` or clone the repo: `git clone https://github.com/johnoseni1/Youtube-video-downloader-forced-`
+2. Download your Youtube subscriptions. Follow the official instructions: https://support.google.com/youtube/answer/6224202?hl=en
+3. Place the downloaded file on the same folder as feedtube
+4. Run `./downloadtube.sh`
+
+That's it! Feedtube will convert the youtube xml file to its own format (`.list.csv`) and keep track of videos.
+
+
+## Running Feedtube
+
+Running `downloadtube.sh` will download new videos for your subscriptions. There's nothing more to it :)
+
+You may run it with cron, or manually. The only caveat is that it doesn't support multiple instances running at the same time. Be careful not to overlap cron executions; give it plenty of time in case it needs to download huge files.
+
+Downloaded filenames have the format: `[Channel name] - Video name`. File timestamp is set by `youtube-dl` as the video upload date.
+
+### Commandline flags
+
+- `-q` Quiet mode. No output is generated, except errors
+- `<Youtube URL>` Single video run. Downloads the selected video and exits. No feeds are parsed.
+
+## Configuration
+
+Edit `feedtube.sh` to access advanced settings:
+
+- `ASCII_ONLY="yes"`: Output video filename will only contain ascii characters. Use for compatibility with some systems/players like Miro. Non-ascii characters will be ignored. This can be a problem in some languages or locales. Set it to `""` (empty) to enable UTF-8 characters in file names.
+
+You can also edit the script to change parameters for `youtube-dl`, like the maximum quality (set to 1080p by default) or video format. [Read youtube-dl documentation](https://github.com/rg3/youtube-dl/blob/master/README.md#readme) for more information.
+
+
+## Adding new subscriptions
+
+Edit `.list.csv` with a text editor and add a new line, with the format:
+
+`Channel title <TAB> channel RSS address`
+
+ Next time you run `downloadtube.sh`, it will download all videos for that channel. If you don't want that (I don't), just Ctrl-C as the download starts. The script will try stop all temporal files.
+
+
+## Watching videos
+
+Downloaded videos can be watched using any standard video player.
+
+I use [Miro](http://www.getmiro.com) to watch videos. I have set it up to monitor the downloads folder for new videos, and lets me organize it by date, size, and others. 
+Furthermore, the video player keeps track of the time you stop watching and lets you resume, like Youtube.
+
+You can also simulate Youtube's sorting options using your OS file explorer:
+
+- Sort by channel: Sort by filename
+- Sort by upload date: Sort by file date
+
+
---
+++ downloadtube.sh
@@ -0,0 +1,89 @@
+
+ASCII_ONLY="yes"  # Output video filename will only contain ascii characters
+
+# Check help switch and show usage
+[[ $1 == "-h" ]] && echo -e "Usage: feedtube.sh [∅ | -q | <youtube URL>]\nTypical usage without parameters. Read the README.md for more information on the usage" && exit
+
+# Check dependencies
+YDL=$(which youtube-dl)
+[[ -z $YDL ]] && echo "Error: missing youtube-dl. Download it here: https://github.com/rg3/youtube-dl/" && exit
+XST=$(which xmlstarlet)
+[[ -z $XST ]] && echo "Error: missing xmlstarlet. Download it here: http://xmlstar.sourceforge.net/" && exit
+
+# If invoked with an URL, download that URL and exit
+if [[ $1 ]] && [[ $1 == http* ]]; then
+    TITLE="$($YDL -e --no-warnings $1)"
+    [[ "$ASCII_ONLY" ]] && TITLE="$(echo $TITLE | iconv -f UTF-8 -t ascii//IGNORE)"
+    echo "Downloading $TITLE..."
+    $YDL -q --no-warnings -f 'bestvideo[ext=mp4+height<=1080]+bestaudio[ext=m4a]' -o "$TITLE" "$1"
+    exit
+fi
+
+# Detect first run
+DOWNLOADED_IDS=.downloaded_ids
+OPML=$(ls *.xml 2> /dev/null)
+LIST_CSV=.list.csv
+if [[ ! -f $DOWNLOADED_IDS ]]; then
+    [[ -z $OPML ]] && echo "First run setup: Missing subscriptions file. Please follow these instructions: https://support.google.com/youtube/answer/6224202?hl=en" && exit
+    [[ -f $LIST_CSV ]] && echo "First run setup: existing $LIST_CSV. Stopping" && exit
+    cat $OPML | sed -e $'s/ \/>/\\\n/g' | sed 's/.*title="\(.*\)" type.*xmlUrl="\(.*\)".*/\1    \2/g'| grep youtube > $LIST_CSV
+    echo -n "" > $DOWNLOADED_IDS
+    FIRST_RUN=1
+    read -p "First run setup: Download latest videos for all subscriptions? This may take a long time/bandwidth (y/N) " DOWNLOAD
+fi
+
+[[ ! -f $LIST_CSV ]] && echo "Error: $LIST_CSV not found. Please run setup again" && exit
+
+while read SUB; do
+    IFS=$'\t' read CHANNEL FEED <<< "$SUB"
+    [[ $1 == "-q" ]] || echo Processing $CHANNEL...
+
+    # Download xml file
+    R_FILE=.$RANDOM.xml
+    wget -q $FEED -O $R_FILE
+    [[ ! -s $R_FILE ]] && echo "Error downloading xml" && rm $R_FILE && continue
+
+    # Parse elements
+    $XST sel -N atom="http://www.w3.org/2005/Atom" -t -m "/atom:feed/atom:entry" -v atom:title -n $R_FILE > $R_FILE.title
+    $XST sel -N atom="http://www.w3.org/2005/Atom" -t -m "/atom:feed/atom:entry/atom:link" -v @href -n $R_FILE > $R_FILE.href
+    $XST sel -N atom="http://www.w3.org/2005/Atom" -t -m "/atom:feed/atom:entry" -v atom:id -n $R_FILE > $R_FILE.id
+    $XST sel -N atom="http://www.w3.org/2005/Atom" -t -m "/atom:feed/atom:entry" -v atom:published -n $R_FILE > $R_FILE.published
+    paste  $R_FILE.id $R_FILE.title $R_FILE.href $R_FILE.published > $R_FILE.pasted
+
+    # Check if videos have been downloaded
+    while read LINE; do
+        IFS=$'\t' read ID TITLE HREF PUBLISHED <<< "$LINE"
+
+        # Check if already downloaded
+        OUT=$(grep "$ID" $DOWNLOADED_IDS)
+        [[ $? -eq 0 ]] && continue
+
+        # Set file name
+        [[ "$ASCII_ONLY" ]] && TITLE="$(echo $TITLE | iconv -f UTF-8 -t ascii//IGNORE)"
+        FILENAME="[$CHANNEL] - $TITLE"
+        FILENAME="$(echo $FILENAME | iconv -f UTF-8 -t ascii//IGNORE)"  # Ignore non-ascii for Miro compatibility
+
+        # Download it
+        shopt -s nocasematch
+        if [[ -z $FIRST_RUN ]] || [[ $DOWNLOAD == "y" ]] ; then
+            [[ $1 == "-q" ]] || echo "Downloading $FILENAME"
+            $YDL  -q --no-warnings -f 'bestvideo[ext=mp4+height<=1080]+bestaudio[ext=m4a]' -o "$FILENAME" "$HREF"
+
+            if [[ $? -eq 0 ]]; then
+                echo -e "$FILENAME\t$ID" >> $DOWNLOADED_IDS  # Mark as downloaded
+	    else
+                rm -f "$1*"
+                read -p "Interrupted. Skip or Retry later? (s/R) " INTERRUPTED < /dev/tty
+                [[ "$INTERRUPTED" == "s" ]] && echo -e "$FILENAME\t$ID" >> $DOWNLOADED_IDS  # Mark as downloaded
+            fi
+        fi
+
+    done < $R_FILE.pasted
+
+    rm $R_FILE*
+
+done < $LIST_CSV
+
+rm -f *.part  # Delete partially downloaded files (errors or Ctrl-C)
+
+[[ $FIRST_RUN ]] && rm $OPML && echo "First run finished. Subsequent runs of this script will download new videos. Enjoy!"
